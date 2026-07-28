ea354709b863e173152e6b61a451f1029332e262
Vicente Bortolini <vicenteb@gmail.com>
2026-07-28 15:50:56 -0300

    Estabiliza tipografia da navegação da home

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,13 @@ export default function RootLayout({
           type="font/woff2"
           crossOrigin="anonymous"
         />
+        <link
+          rel="preload"
+          href="/fonts/montserrat-latin-600-normal.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </head>
       <body>{children}</body>
     </html>
