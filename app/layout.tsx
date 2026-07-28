2e3d462a30a5d04b799b6acaa2f0b68d5f809d03
Vicente Bortolini <vicenteb@gmail.com>
2026-07-28 15:40:03 -0300

    Evita troca visual no título da home

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link
+          rel="preload"
+          href="/fonts/montserrat-latin-400-normal.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/montserrat-latin-500-normal.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
