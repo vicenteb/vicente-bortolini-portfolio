7c86783b3de91eb6f6c171fa9e3a4bbacb9705c8
Vicente Bortolini <vicenteb@gmail.com>
2026-08-08 16:58:32 -0300

    Load mobile portfolio refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./mobile-overrides.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://vicentebortolini.com"),
